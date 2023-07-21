7a29afe0b9c58b9a6ed53dfd7e075f93700a6a44
aspyn1 <asypnisgood@gamil.com>
2023-07-21 17:35:34 +0900

    commit

--- src/edu/kh/array/practice/run/PracticeRun.java
+++ src/edu/kh/array/practice/run/PracticeRun.java
@@ -7,12 +7,19 @@ public class PracticeRun {
 		PracticeService practice = new PracticeService();
 		
 		//practice.ex1();
-		//practice.ex2();
+		practice.ex2();
 		//practice.ex3();
 		//practice.ex4();
 		//practice.ex5();
 		//practice.ex6();
-		practice.ex7();
+		//practice.ex7();
+		//practice.ex8();
+		//practice.ex9();
+		//practice.ex10();
+		//practice.ex11();
+		//practice.ex12();
+		//practice.ex13();
+		//practice.ex13();
 		
 		
 
--- src/edu/kh/array/practice/run/PracticeService.java
+++ src/edu/kh/array/practice/run/PracticeService.java
@@ -1,6 +1,7 @@
 package edu.kh.array.practice.run;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class PracticeService {
@@ -32,7 +33,7 @@ public class PracticeService {
 	}
 
 
-	public void ex2() {//완성x
+	public void ex2() {
 		/*
 		 * 길이가 9인 배열을 선언 및 할당하고, 9부터 1까지의 값을 반복문을 이용하여
 			순서대로 배열의 각 인덱스 요소에 대입하고 출력한 후
@@ -47,10 +48,8 @@ public class PracticeService {
 		
 		for(int i = 0; i < 9; i++) {
 			
-			for(int x = 9; x >= 1; x--) {
-				arr[i] = x;
-			}
-			
+			arr[i] = 9-i;
+		
 			if(i % 2 != 0) {
 				sum += arr[i];
 			}
@@ -242,41 +241,222 @@ public class PracticeService {
 	}
 
 	
-	public void ex8() {
+	public void ex8() {  //완성x
+		/*
+		 * 3이상인 홀수를 입력 받아 
+		 * 배열의 중간까지는 1부터 1씩 증가하여 오름차순으로 값을 넣고,
+		중간 이후부터 끝까지는 1씩 감소하여 내림차순으로 값을 넣어 출력하세요.
 		
+		단, 입력한 정수가 홀수가 아니거나 3 미만일 경우 “다시 입력하세요”를 출력하고
+		다시 정수를 받도록 하세요.
+		
+		[실행 화면]
+		정수 : 4
+		다시 입력하세요.
+		정수 : -6
+		다시 입력하세요.
+		정수 : 5
+		1, 2, 3, 2, 1
+		 */
+		
+		System.out.print("정수 : ");
+		int input = sc.nextInt();
+		
+		int[] arr = new int[input];
+		
+		if(input < 3 || input % 2 == 0) {
+			System.out.println("다시 입력하세요.");
+		}else if(input >= 3 && input % 2 != 0) {
+			
+			for(int i = 0; i < arr.length; i++){
+				
+				if(i <= arr.length/2+1) {
+					arr[i] = i;
+					Arrays.sort(arr);
+				}else {
+					arr[i] = i-1;
+				}
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+ 	}
+
+	
+	public void ex9() { // 질문 : 랜덤 선언은 왜 밖에다 하면 안되고 포문 안에해야되는건가
+		/*
+		 * 10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고,
+			1~10 사이의 난수를 발생시켜 배열에 초기화한 후 출력하세요.
+			
+			[실행 화면]
+			발생한 난수 : 9 7 6 2 5 10 7 2 9 6
+		 */
+		
+		int[] arr = new int[10];
+		
+		for(int i = 0; i < arr.length; i++) {
+
+			int random = (int)(Math.random() * 10 + 1);
+			arr[i] = random;
+		}
+		
+		System.out.println(Arrays.toString(arr));
 	}
 
 	
-	public void ex9() {
+	public void ex10() { // min값이 이상함
+		/*
+		 * 10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고,
+			1~10 사이의 난수를 발생시켜 배열에 초기화 후
+			배열 전체 값과 그 값 중에서 최대값과 최소값을 출력하세요.
+			
+			[실행 화면]
+			발생한 난수 : 5 3 2 7 4 8 6 10 9 10
+			최대값 : 10
+			최소값 : 2
+		 */
 		
-	}
+		int[] arr = new int[10];
+		int max = arr[0];
+		int min = arr[0];
+		
+		for(int i =0; i < arr.length; i++) {
+		
+			int random = (int)(Math.random() * 10 + 1);
+			arr[i] = random;
+			
+			if(max <= arr[i]) {
+				max = arr[i];
+			}
+			if(min >= arr[i]) {
+				min = arr[i];
+			}
+		}
+		
+		System.out.println("발생한 난수 : " + Arrays.toString(arr));
+		System.out.println("최대값 : " + max);
+		System.out.println("최소값 : " + min);
 
-	
-	public void ex10() {
-		
 	}
 
 	
 	public void ex11() {
+		/*
+		 * 10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고
+			1~10 사이의 난수를 발생시켜 중복된 값이 없게 배열에 초기화한 후 출력하세요.
+			
+			[실행 화면]
+			4 1 3 6 9 5 8 10 7 2
+		 */
 		
+		int[] arr = new int[10];
+		
+		for(int i = 0; i < arr.length; i++) {
+			int random = (int)(Math.random() * 10 + 1);
+			arr[i] = random;
+			
+			for(int x = 0; x < i; x++) {
+				if(random == arr[x]) {
+					i--;
+					break;
+				}
+			}
+		}
+
+		System.out.println(Arrays.toString(arr));
 	}
 
 	
 	public void ex12() {
+		/*
+		 * 로또 번호 자동 생성기 프로그램을 만들기.
+			(중복 값 없이 오름차순으로 정렬하여 출력하세요.)
+			
+			[실행 화면]
+			3 4 15 17 28 40
+		 */
 		
+		int[] lotto = new int[6];
+		
+		for(int i = 0; i < lotto.length; i++) {
+			int random = (int)(Math.random() * 45 + 1);
+			lotto[i] = random;
+		}
+		Arrays.sort(lotto);
+		System.out.println(Arrays.toString(lotto));
 	}
 
 	
 	public void ex13() {
+		/*
+		 * 문자열을 입력 받아 문자열에 어떤 문자가 들어갔는지 배열에 저장하고
+			문자의 개수와 함께 출력하세요.
+			
+			[실행 화면]
+			문자열 : application
+			문자열에 있는 문자 : a, p, l, i, c, t, o, n
+			문자 개수 : 8
+		 */
 		
+		System.out.print("문자열 : ");
+		String input = sc.next();
+		char[] arr = new char[input.length()];
+		int count = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = input.charAt(i);
+			count++;
+		}
+		
+		System.out.print("문자열에 있는 문자 : ");
+		for(int i = 0; i < arr.length; i++) {
+			
+			if(i+1 < arr.length) {
+				System.out.print( arr[i] + ", ");
+			}else {
+				System.out.print( arr[i]);
+			}
+		}
+		System.out.print("\n" + "문자 개수 : " + count);
 	}
 
 	
-	public void ex14() {
+	public void ex14() { // 완성x
+		/*
+		 * 사용자가 입력한 배열의 길이만큼의 문자열 배열을 선언 및 할당하고
+			배열의 인덱스에 넣을 값 역시 사용자가 입력하여 초기화 하세요.
+			
+			단, 사용자에게 배열에 값을 더 넣을지 물어보고 몇 개를 더 입력할 건지,
+			늘린 곳에 어떤 데이터를 넣을 것인지 받으세요.
+			
+			사용자가 더 이상 입력하지 않겠다고 하면 배열 전체 값을 출력하세요.
+		 */
 		
+		System.out.print("배열의 크기를 입력하세요 : ");
+		int input = sc.nextInt();
+		
+		String[] arr = new String[input];
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(i+1 + "번째 문자열 : ");
+			String str = sc.nextLine();
+			
+			arr[i] = str;
+			
+			if(i+1 == input) {
+				
+				System.out.print("더 값을 입력하시겠습니까?(Y/N) : ");
+				char yesno = sc.next().charAt(0);
+				
+				if(yesno == 'y') { // 데이터 늘리기 배열 복사..? 늘리기..?
+					System.out.print("더 입력하고 싶은 개수 : ");
+					int input2 = sc.nextInt();
+					
+					
+				}else {
+					System.out.println(Arrays.toString(arr));
+				}
+			}
+		}
 	}
-
-
-	
 
 }
